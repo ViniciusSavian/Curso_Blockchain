bc4470e3a314a08bb555e161c022dc05b9df8e93
ViniciusSavian <vinicius.savian@sou.inteli.edu.br>
2024-03-07 19:44:14 -0300

    Atualização

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex border-2 justify between ">
+      <h1 className="text-bold text-blue-300">Hello, world!</h1>
+      <p className = "texte-underline">Olá Inteli</p>
     </div>
   );
 }
