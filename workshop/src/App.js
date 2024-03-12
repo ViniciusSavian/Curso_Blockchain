133f657eb090152c34940d32881c4960bb444dbd
ViniciusSavian <vinicius.savian@sou.inteli.edu.br>
2024-03-12 19:42:38 -0300

    atualização

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import Input from "./components/Input";
 function App() {
   return (
-    <div className="flex border-2 justify between ">
-      <h1 className="text-bold text-blue-300">Hello, world!</h1>
-      <p className = "texte-underline">Olá Inteli</p>
+    <div className="flex min-h-screen w-full h-full items-center justify-center">
+      <Input/>
     </div>
   );
 }
